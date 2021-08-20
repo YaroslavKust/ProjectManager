aa2568837ee16a8d238746d2dc5f693d46fc98d9
YaroslavKust <79914649+YaroslavKust@users.noreply.github.com>
2021-08-20 22:05:34 +0300

    Add DI

---
+++ ProjectManager.BL/Infrastructure/ServiceModule.cs
@@ -0,0 +1,20 @@
+﻿using Ninject.Modules;
+using ProjectManager.DAL.UnitOfWorks;
+
+namespace ProjectManager.BL.Infrastructure
+{
+    public class ServiceModule: NinjectModule
+    {
+        private string _connection;
+
+        public ServiceModule(string connection)
+        {
+            _connection = connection;
+        }
+
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<UnitOfWork>().WithConstructorArgument(_connection);
+        }
+    }
+}
